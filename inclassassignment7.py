d8c3ec2842f742c4c4af73a0fec19a488f0edacc
Mark T. Johnston <X00020132@wcc.dom>
2019-03-07 11:50:18 -0600

    File modified to add a multiplication display

--- inclassassignment7.py
+++ inclassassignment7.py
@@ -2,4 +2,4 @@ numba_one = int(input("Enter first number: "))
 numba_two = int(input("Enter second number: "))
 
 print("The quotient is", numba_one//numba_two, "remainder", numba_one % numba_two)
-
+print("The product is", numba_one * numba_two)
