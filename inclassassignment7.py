7b301ef4ef98dacae1cb97c5f0ae3c637844c11e
Mark T. Johnston <X00020132@wcc.dom>
2019-03-07 11:49:38 -0600

    Initial division remainder entry

---
+++ inclassassignment7.py
@@ -0,0 +1,5 @@
+numba_one = int(input("Enter first number: "))
+numba_two = int(input("Enter second number: "))
+
+print("The quotient is", numba_one//numba_two, "remainder", numba_one % numba_two)
+
